86e9f31cb8e291d8c5d48e229405ef370315e2be
talnoznisky <tal.noznisky@gmail.com>
2019-06-30 23:07:00 -0500

    got it reading the different things off

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 1. ACCESSES TOP WIKIPEDIA PAGES IN THE LAST HOUR FROM http://www.wikitrends.net/
 2. LOGS THE TOPICS AND VIEWS EACH PAGE TO SCRAPE THE CATEGORIES
 3. RECITES/PRINTS THE TOPICS AND THEIR CATEGORIES
+
+
+Play this in the background https://www.youtube.com/watch?v=xy9r0vhOVpk
--- get-pages.py
+++ get-pages.py
@@ -1,23 +1,27 @@
 import requests
 import wikipedia
+import pyttsx3
 from BeautifulSoup import BeautifulSoup
 
-
-links = []
 topics = []
 data = []
 
 url = 'http://www.wikitrends.net/'
 soup = BeautifulSoup(requests.get(url).content)
 
-
 for d in soup.findAll('div', {'class':'col-md-7 col-xs-5'}):
     for b in d.findAll('b'):
         topics.append(b.contents)
 
 for a in topics[0]:
-    title = a
+    title = a.encode('utf-8')
     categories =  wikipedia.WikipediaPage(title=a).categories
+    for i in categories:
+        i.encode('utf-8')
     data.append([title,categories])
 
-print data
+for i in topics[0:2]:
+    text = ('The topic is %s and its categories are %s' %(data[0][0], ", ".join(data[0][1])))
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
